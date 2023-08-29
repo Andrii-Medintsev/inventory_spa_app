ff3f2b56b7bf9d7b395c2ff898e91dadec6e2258
Andrii Medintsev <medintsev.andrew@gmail.com>
2023-08-29 17:04:18 +0300

    Header was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.scss';
+import { ReactComponent as Shield } from './images/shield-half.svg';
+import { ReactComponent as Clock } from './images/time.svg';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1 className='text-primary'>Hello, Inventory</h1>
+    <div className="App text-secondary">
+      <header className="header shadow-lg p-2">
+        <div className="header__container container d-flex justify-content-between align-items-center p-0">
+          <div className="header__left d-flex align-items-center">
+            <div className="header__logo d-flex align-items-center gap-4">
+              <Shield width={35} />
+              <span className='header__logo-text text-uppercase'>Inventory</span>
+            </div>
+            <form action="/" method="get" className='d-inline-block ms-5'>
+              <input className="header__input bg-light form-control me-2 py-1" type="search" placeholder="Поиск" aria-label="Search" />
+            </form>
+          </div>
+
+          <div className="header__date-time d-flex flex-column">
+            <span className="header__day-name">Вторник</span>
+            <span className="header__date d-flex gap-3">06 Апр, 2017
+              <span className='header__time d-flex align-items-center gap-1'>
+                <Clock width={12} height={12} />
+                17:20
+              </span>
+            </span>
+          </div>
+        </div>
+      </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route>
+//       <Route
+//         path='/'
+//         element={<App />}
+//       />
+//     </Route>
+//   )
+// );
+
+// root.render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
+const Root = () => (
+  <Router>
+    <App />
+  </Router>
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Root />);
